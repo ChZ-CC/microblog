2641d5d4739e77f0f87e30841912254f8f55ec29
ChZ <wsndcc@yeah.net>
2018-04-01 21:29:51 +0800

    first version. chapter 1 to 14.

---
+++ config.py
@@ -0,0 +1,42 @@
+#-*- encoding: utf-8 -*-
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    # 激活 跨站点请求伪造 保护
+    #CSRF_ENABLED = True 
+    # 当 CSRF 激活时，需要配置 SECRET_KEY，用于验证表单的一个加密令牌。
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # SQLALCHEMY_DATABASE_URI 是 Flask-SQLAlchemy 扩展需要的，是数据库文件路径
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # QLALCHEMY_MIGRATE_REPO 是存放 SQLAlchemy-migrate 数据文件的文件夹。
+    #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+    #SQLALCHEMY_COMMIT_ON_TEARDOW = True
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    #MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['testforflask@163.com']
+    LANGUAGES = ['en', 'zh']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    POSTS_PER_PAGE = 3
+
+
+# 大的互联网服务提供商支持 OpenID 认证自己的会员
+#OPENID_PROVIDERS = [
+#    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+#    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+#    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+#    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+#    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+# 数据库配置
+
